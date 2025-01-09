a930ea4a9f58d2ef9094373340b4fb651fc2575b
mohamed130761 <mohamed130761@gmail.com>
2025-01-10 00:35:54 +0200

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return render(request,'pages/home.html')
+
+def about(request):
+    return render(request,'pages/about.html')
+
+def navbar(request):
+    return render(request,'parts/navbar.html')
