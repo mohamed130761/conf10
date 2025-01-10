fa94a53f8a8b97b219f1275e9cac96838615aba2
mohamed130761 <mohamed130761@gmail.com>
2025-01-10 21:42:36 +0200

    initial commit

--- website/settings.py
+++ website/settings.py
@@ -142,23 +142,3 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'website/static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'django_debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
-
-CSRF_TRUSTED_ORIGINS = ['https://conf10.vercel.app']
